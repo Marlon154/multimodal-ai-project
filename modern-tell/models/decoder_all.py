6f411e31ac8655cac9cbd4fcc9cd749ead399bdf
Laurenz <laurenz.kammeyer@gmx.de>
2024-06-18 16:24:50 +0200

    I am pretty sure this is where this needs to start

---
+++ modern-tell/models/decoder_all.py
@@ -0,0 +1,8 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from .decoder_base import Decoder, DecoderLayer
+
+class DynamicConvFacesObjectsDecoder(Decoder):
+    pass
---
+++ modern-tell/models/decoder_base.py
@@ -0,0 +1,9 @@
+import torch.nn as nn
+
+
+class Decoder(nn.Module):
+    pass
+
+
+class DecoderLayer(nn.Module):
+    pass
