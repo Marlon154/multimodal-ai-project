d4ee788990ae0ca17277ac4c66119acd9363b8b4
Laurenz Kammeyer <laurenz.kammeyer@gmx.de>
2024-06-25 21:03:44 +0200

    added resnet places model

--- src/scene_embedder.py
+++ src/scene_embedder.py
@@ -1,17 +1,49 @@
 import torch
 import torchvision.transforms as transforms
+from transformers import AutoFeatureExtractor, ResNetForImageClassification
 from PIL import Image
 import torchfile
+from pymongo import MongoClient
+from PIL import Image
+import os
+
+# DO NOT TOUCH
+from resnet152Places365 import resnet152Places365
 
 
-def load_t7_model(t7_model_path):
-    # Load the .t7 model
-    t7_model = torchfile.load(t7_model_path)
+def connect():
+    client = MongoClient("mongodb://root:secure_pw@localhost:27017/")
+    return client
 
-    # Convert the .t7 model to PyTorch (assuming the Torch7 model can be loaded directly)
-    # Here, we should normally convert layers manually, but assuming it is directly compatible
-    model = torch.jit.load(t7_model_path)
 
+def get_imgages(
+    db="nytimes",
+    image_folder="./../input_data/uncompressed/images_processed/",
+    image_extension=".jpg",
+    sample=None,
+):
+    # print("get_images...")
+    client = connect()
+
+    # print("Getting correct collection...")
+    db = client["nytimes_sample"]
+    image_table = db["images"]
+
+    # print("Finding images...")
+    ids = []
+    # if sample
+    documents = image_table.find().limit(sample)
+    for doc in documents:
+        ids.append(os.path.join(image_folder, doc["_id"] + image_extension))
+
+    # print(f"{ids=}")
+    return ids
+
+
+def load_model():
+    model = resnet152Places365.resnet152_places365
+    model.load_state_dict(torch.load("./src/resnet152Places365/resnet152Places365.pth"))
+    model.eval()
     return model
 
 
@@ -43,8 +75,8 @@ def predict(model, image_tensor):
     return outputs
 
 
-def main(t7_model_path, image_path):
-    model = load_t7_model(t7_model_path)
+def main(image_path):
+    model = load_model()
 
     # Preprocess the image
     image_tensor = preprocess_image(image_path)
@@ -57,6 +89,8 @@ def main(t7_model_path, image_path):
 
 
 if __name__ == "__main__":
-    t7_model_path = "path_to_your_resnet152.t7"
-    image_path = "path_to_your_image.jpg"
-    main(t7_model_path, image_path)
+    # image_path = get_imgages(
+    #     image_folder="./data/nytimes/sample/sample_images/", sample=1
+    # )[0]
+    load_model()
+    # main(image_path)
