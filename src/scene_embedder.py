e2a4421d78c4e182f0dbc7458ed853cae0626cee
Laurenz Kammeyer <laurenz.kammeyer@gmx.de>
2024-06-26 16:07:44 +0200

    getting embeddings works. Now try to make it skalable to many images efficiently

--- src/scene_embedder.py
+++ src/scene_embedder.py
@@ -2,13 +2,36 @@ import torch
 import torchvision.transforms as transforms
 from transformers import AutoFeatureExtractor, ResNetForImageClassification
 from PIL import Image
-import torchfile
 from pymongo import MongoClient
 from PIL import Image
 import os
 
 # DO NOT TOUCH
 from resnet152Places365 import resnet152Places365
+
+
+class ActivationExtractor:
+    def __init__(self, model: torch.nn.Sequential, layer_from_end):
+        self.model = model
+        model.eval()
+        self.layer_from_end = layer_from_end
+        self.hook_handle = None
+        self.activation = None
+
+        self.layer_index = len(list(model.children())) - layer_from_end
+        if self.layer_index < 0:
+            raise ValueError(f"The model does not have {layer_from_end} layers")
+
+        self.target_layer = list(model.children())[self.layer_index]
+        self.hook_handle = self.target_layer.register_forward_hook(self.hook_fn)
+
+    def hook_fn(self, module, input, output):
+        self.activation = output
+
+    def get_activation(self, input):
+        with torch.no_grad():
+            _ = self.model(input)
+        return self.activation
 
 
 def connect():
@@ -47,7 +70,7 @@ def load_model():
     return model
 
 
-def preprocess_image(image_path):
+def preprocess_image(image: Image):
     # transform for image
     preprocess = transforms.Compose(
         [
@@ -58,8 +81,6 @@ def preprocess_image(image_path):
         ]
     )
 
-    image = Image.open(image_path)
-
     # Apply the transformation
     image = preprocess(image)
     image = image.unsqueeze(0)  # Add batch dimension
@@ -67,30 +88,21 @@ def preprocess_image(image_path):
     return image
 
 
-def predict(model, image_tensor):
-    model.eval()
-    with torch.no_grad():
-        outputs = model(image_tensor)
-
-    return outputs
-
-
-def main(image):
+def get_embedding(images: Image, layer_from_end=5) -> torch.tensor:
     model = load_model()
+    activation_extractor = ActivationExtractor(model, layer_from_end=layer_from_end)
 
     # Preprocess the image
-    image_tensor = preprocess_image(image)
+    image_tensor = preprocess_image(images)
 
-    # Run the prediction
-    outputs = predict(model, image_tensor)
+    embeddings = activation_extractor.get_activation(image_tensor)
 
-    return outputs
+    return embeddings
 
 
 if __name__ == "__main__":
-    image = get_imgages(image_folder="./data/nytimes/sample/sample_images/", sample=1)[
-        0
-    ]
-    Image.open(image).show()
-    # load_model()
-    print(main(image).argmax())
+    image_path = get_imgages(image_folder="./data/nytimes/sample/sample_images/", sample=1)[0]
+    image = Image.open(image_path)
+    for i in range(1, 5):
+        embedding = get_embedding(image, i)
+        print(f"layer:{i}  {embedding.size()=}")
