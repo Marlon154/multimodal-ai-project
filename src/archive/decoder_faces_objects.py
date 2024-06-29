3b25a650ecb3f08e56db8a33bf9eabdc0ad77fe8
j-milkovits <j.milkovits.t@posteo.net>
2024-06-29 13:44:37 +0200

    create archive folder and move old code there

--- src/archive/decoder_faces_objects.py
+++ src/archive/decoder_faces_objects.py
@@ -1,23 +1,50 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from transformers import PreTrainedModel, PretrainedConfig
+from transformers import PretrainedConfig, PreTrainedModel
 from transformers.modeling_outputs import BaseModelOutput
 from transformers.models.bert.modeling_bert import BertEmbeddings
+
 from modules import (AdaptiveSoftmax, DynamicConv1dTBC, GehringLinear,
-                          LightweightConv1dTBC, MultiHeadAttention)
+                     LightweightConv1dTBC, MultiHeadAttention)
 
 
 class DynamicConvDecoderConfig(PretrainedConfig):
-    def __init__(self, vocab_size, max_position_embeddings, hidden_size, num_attention_heads,
-                 intermediate_size, hidden_dropout_prob, attention_probs_dropout_prob,
-                 max_target_positions, dropout, share_decoder_input_output_embed,
-                 decoder_output_dim, decoder_conv_dim, decoder_glu,
-                 decoder_conv_type, weight_softmax, weight_dropout, relu_dropout, input_dropout,
-                 decoder_normalize_before, decoder_kernel_size_list, adaptive_softmax_cutoff,
-                 tie_adaptive_weights, adaptive_softmax_dropout, tie_adaptive_proj,
-                 adaptive_softmax_factor, decoder_layers, final_norm, padding_idx,
-                 namespace, section_attn, article_embed_size, **kwargs):
+    def __init__(
+        self,
+        vocab_size,
+        max_position_embeddings,
+        hidden_size,
+        num_attention_heads,
+        intermediate_size,
+        hidden_dropout_prob,
+        attention_probs_dropout_prob,
+        max_target_positions,
+        dropout,
+        share_decoder_input_output_embed,
+        decoder_output_dim,
+        decoder_conv_dim,
+        decoder_glu,
+        decoder_conv_type,
+        weight_softmax,
+        weight_dropout,
+        relu_dropout,
+        input_dropout,
+        decoder_normalize_before,
+        decoder_kernel_size_list,
+        adaptive_softmax_cutoff,
+        tie_adaptive_weights,
+        adaptive_softmax_dropout,
+        tie_adaptive_proj,
+        adaptive_softmax_factor,
+        decoder_layers,
+        final_norm,
+        padding_idx,
+        namespace,
+        section_attn,
+        article_embed_size,
+        **kwargs,
+    ):
         super().__init__(**kwargs)
         self.vocab_size = vocab_size
         self.max_position_embeddings = max_position_embeddings
@@ -56,9 +83,13 @@ class DynamicConvDecoder(nn.Module):
     config_class = DynamicConvDecoderConfig
 
     def __init__(self, config):
-        self.embeddings = BertEmbeddings(config)   # todo p4 make generic
-        self.layers = nn.ModuleList([DynamicConvDecoderLayer(config, kernel_size=config.decoder_kernel_size_list[i])
-                                     for i in range(config.decoder_layers)])
+        self.embeddings = BertEmbeddings(config)  # todo p4 make generic
+        self.layers = nn.ModuleList(
+            [
+                DynamicConvDecoderLayer(config, kernel_size=config.decoder_kernel_size_list[i])
+                for i in range(config.decoder_layers)
+            ]
+        )
         self.layer_norm = nn.LayerNorm(config.hidden_size) if config.final_norm else None
         self.dropout = nn.Dropout(config.dropout)
 
@@ -80,12 +111,12 @@ class DynamicConvDecoder(nn.Module):
 
         if config.adaptive_softmax_cutoff is not None:
             adaptive_inputs = None
-            if isinstance(self.embeddings, BertEmbeddings):   # todo likely bug here
+            if isinstance(self.embeddings, BertEmbeddings):  # todo likely bug here
                 adaptive_inputs = self.embeddings
-            elif hasattr(self.embeddings, 'token_embedder_adaptive'):
+            elif hasattr(self.embeddings, "token_embedder_adaptive"):
                 adaptive_inputs = self.embeddings.token_embedder_adaptive
             elif config.tie_adaptive_weights:
-                raise ValueError('Cannot locate adaptive_inputs.')
+                raise ValueError("Cannot locate adaptive_inputs.")
             self.adaptive_softmax = AdaptiveSoftmax(
                 config.vocab_size,
                 output_embed_dim,
@@ -97,9 +128,9 @@ class DynamicConvDecoder(nn.Module):
             )
         elif not config.share_decoder_input_output_embed:
             self.embed_out = nn.Parameter(torch.Tensor(config.vocab_size, output_embed_dim))
-            nn.init.normal_(self.embed_out, mean=0, std=output_embed_dim ** -0.5)
+            nn.init.normal_(self.embed_out, mean=0, std=output_embed_dim**-0.5)
 
-        self.register_buffer('version', torch.tensor([2], dtype=torch.float32))
+        self.register_buffer("version", torch.tensor([2], dtype=torch.float32))
         self.normalize = config.decoder_normalize_before and config.final_norm
         if self.normalize:
             self.layer_norm = nn.LayerNorm(embed_dim)
@@ -136,16 +167,24 @@ class DynamicConvDecoderLayer(nn.Module):
             self.linear1 = GehringLinear(self.embed_dim, self.conv_dim)
             self.act = None
 
-        if config.decoder_conv_type == 'lightweight':
-            self.conv = LightweightConv1dTBC(self.conv_dim, kernel_size, padding_l=kernel_size-1,
-                                             weight_softmax=config.weight_softmax,
-                                             num_heads=config.num_attention_heads,
-                                             weight_dropout=config.weight_dropout)
-        elif config.decoder_conv_type == 'dynamic':
-            self.conv = DynamicConv1dTBC(self.conv_dim, kernel_size, padding_l=kernel_size-1,
-                                         weight_softmax=config.weight_softmax,
-                                         num_heads=config.num_attention_heads,
-                                         weight_dropout=config.weight_dropout)
+        if config.decoder_conv_type == "lightweight":
+            self.conv = LightweightConv1dTBC(
+                self.conv_dim,
+                kernel_size,
+                padding_l=kernel_size - 1,
+                weight_softmax=config.weight_softmax,
+                num_heads=config.num_attention_heads,
+                weight_dropout=config.weight_dropout,
+            )
+        elif config.decoder_conv_type == "dynamic":
+            self.conv = DynamicConv1dTBC(
+                self.conv_dim,
+                kernel_size,
+                padding_l=kernel_size - 1,
+                weight_softmax=config.weight_softmax,
+                num_heads=config.num_attention_heads,
+                weight_dropout=config.weight_dropout,
+            )
         else:
             raise NotImplementedError
 
@@ -162,27 +201,39 @@ class DynamicConvDecoderLayer(nn.Module):
         self.context_attn_lns = nn.ModuleDict()
 
         # todo make generic
-        self.context_attns['image'] = MultiHeadAttention(
-            self.embed_dim, config.num_attention_heads, kdim=2048, vdim=2048,
-            dropout=config.attention_probs_dropout_prob)
-        self.context_attn_lns['image'] = nn.LayerNorm(self.embed_dim)
+        self.context_attns["image"] = MultiHeadAttention(
+            self.embed_dim,
+            config.num_attention_heads,
+            kdim=2048,
+            vdim=2048,
+            dropout=config.attention_probs_dropout_prob,
+        )
+        self.context_attn_lns["image"] = nn.LayerNorm(self.embed_dim)
 
-        self.context_attns['article'] = MultiHeadAttention(
-            self.embed_dim, config.num_attention_heads, kdim=config.article_embed_size, vdim=config.article_embed_size,
-            dropout=config.attention_probs_dropout_prob)
-        self.context_attn_lns['article'] = nn.LayerNorm(self.embed_dim)
+        self.context_attns["article"] = MultiHeadAttention(
+            self.embed_dim,
+            config.num_attention_heads,
+            kdim=config.article_embed_size,
+            vdim=config.article_embed_size,
+            dropout=config.attention_probs_dropout_prob,
+        )
+        self.context_attn_lns["article"] = nn.LayerNorm(self.embed_dim)
 
-        self.context_attns['faces'] = MultiHeadAttention(
-            self.embed_dim, config.num_attention_heads, kdim=512, vdim=512,
-            dropout=config.attention_probs_dropout_prob)
-        self.context_attn_lns['faces'] = nn.LayerNorm(self.embed_dim)
+        self.context_attns["faces"] = MultiHeadAttention(
+            self.embed_dim, config.num_attention_heads, kdim=512, vdim=512, dropout=config.attention_probs_dropout_prob
+        )
+        self.context_attn_lns["faces"] = nn.LayerNorm(self.embed_dim)
 
-        self.context_attns['obj'] = MultiHeadAttention(
-            self.embed_dim, config.num_attention_heads, kdim=2048, vdim=2048,
-            dropout=config.attention_probs_dropout_prob)
-        self.context_attn_lns['obj'] = nn.LayerNorm(self.embed_dim)
+        self.context_attns["obj"] = MultiHeadAttention(
+            self.embed_dim,
+            config.num_attention_heads,
+            kdim=2048,
+            vdim=2048,
+            dropout=config.attention_probs_dropout_prob,
+        )
+        self.context_attn_lns["obj"] = nn.LayerNorm(self.embed_dim)
 
-        context_size = self.embed_dim * 4   # maybe 2
+        context_size = self.embed_dim * 4  # maybe 2
 
         self.context_fc = GehringLinear(context_size, self.embed_dim)
 
@@ -208,16 +259,18 @@ class DynamicConvDecoderLayer(nn.Module):
         attns = {}
         X_contexts = []
 
-        for context_type in ['image', 'article', 'faces', 'obj']:
+        for context_type in ["image", "article", "faces", "obj"]:
             residual = X
             X_context = self.maybe_layer_norm(self.context_attn_lns[context_type], X, before=True)
-            X_context, attn = self.context_attns[context_type](query=X_context,
-                                                               key=contexts[context_type],
-                                                               value=contexts[context_type],
-                                                               key_padding_mask=contexts[context_type + '_mask'],
-                                                               incremental_state=None,
-                                                               static_kv=True,
-                                                               need_weights=(not self.training and self.need_attn))
+            X_context, attn = self.context_attns[context_type](
+                query=X_context,
+                key=contexts[context_type],
+                value=contexts[context_type],
+                key_padding_mask=contexts[context_type + "_mask"],
+                incremental_state=None,
+                static_kv=True,
+                need_weights=(not self.training and self.need_attn),
+            )
             X_context = F.dropout(X_context, p=self.dropout, training=self.training)
             X_context = residual + X_context
             X_context = self.maybe_layer_norm(self.context_attn_lns[context_type], X_context, after=True)
@@ -250,4 +303,4 @@ class DynamicConvDecoderLayer(nn.Module):
         self.need_attn = need_attn
 
     def extra_repr(self):
-        return f'dropout={self.dropout}, relu_dropout={self.relu_dropout}, input_dropout={self.input_dropout}, normalize_before={self.normalize_before}'
+        return f"dropout={self.dropout}, relu_dropout={self.relu_dropout}, input_dropout={self.input_dropout}, normalize_before={self.normalize_before}"
